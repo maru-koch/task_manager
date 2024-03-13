ab506f68562484444ee7366092a20cb50fd059d9
= <=>
2024-03-13 05:05:51 -0700

    Page Number Pagination
    paginated the endpoint that returns the list of tasks

--- backend/core/views.py
+++ backend/core/views.py
@@ -3,6 +3,7 @@ from rest_framework.generics import (
     RetrieveUpdateDestroyAPIView)
 from django.contrib.auth.mixins import LoginRequiredMixin
 from rest_framework.views import APIView
+from rest_framework.pagination import PageNumberPagination
 from core.serializer import TaskSerializer
 from rest_framework.response import Response
 from rest_framework import status
@@ -12,6 +13,7 @@ from core.models import Task
 class TasksView(ListCreateAPIView, LoginRequiredMixin):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
+    pagination_class = PageNumberPagination
 
     def get_queryset(self):
         """Returns only the login user's tasks 
